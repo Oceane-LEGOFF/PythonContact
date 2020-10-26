5c996e41e7529ea1da77e7711e0185741c02800a
Océane <oceanekegoff@gmail.com>
2020-10-26 11:21:58 +0100

    Premier Commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from art import *
+
+def say_hello(name):
+        print("Hello " + name)
+        print(f"How are you {name} ?")
+
+#n = "Bob"
+h = art("Hello")
+print(h)
+print(text2art("YO ' ROBIN", font="block"))
