8f8adea253a6793f8687242e415633ef4e215a54
Océane <oceanekegoff@gmail.com>
2020-10-26 11:42:50 +0100

    Petite modif sur main.py

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ def say_hello(name):
         print("Hello " + name)
         print(f"How are you {name} ?")
 
-#n = "Bob"
+n = "Bob"
+say_hello(n)
 h = art("Hello")
 print(h)
 print(text2art("YO ' ROBIN", font="block"))
